39f9913d53b7424ede2bf99bb4a4aab6ce274127
Saurbh Shukla <ankitshukla2024@gmail.com>
2021-04-05 20:24:50 +0530

    refactored and added login and signup functionality

--- env/app.py
+++ env/app.py
@@ -33,60 +33,116 @@ class ChatBot(db.Model):
 	def __repr__(self):
 		return '<Task %r>' % self.id
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 
 def index():
 
+	# if request.method == 'POST':
+	# 	task_content = request.form['content']
+	# 	new_task = Todo(content=task_content)
+
+	# 	try:
+	# 		db.session.add(new_task)
+	# 		db.session.commit()
+
+	# 		return redirect('/')
+
+	# 	except:
+	# 		return 'Database Error'
+
+	# else:
+
+	# 	tasks = Todo.query.order_by(Todo.date_created).all()
+
+		return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+	# task_to_delete = Todo.query.get_or_404(id)
+
+	# try:
+	# 	db.session.delete(task_to_delete)
+	# 	db.session.commit()
+
+	# 	return redirect('/')
+
+	# except:
 	if request.method == 'POST':
-		task_content = request.form['content']
-		new_task = Todo(content=task_content)
+		if request.form['usertype'] == 'admin':
+			admin = Admin.query.filter_by(admin=request.form['username']).first()
+			password = Admin.query.filter_by(password=request.form['password']).first()
+			if admin.id==password.id:
+				return redirect('/admin')
+			else:
+				return "incorrect username password pair"
 
-		try:
-			db.session.add(new_task)
-			db.session.commit()
-
-			return redirect('/')
-
-		except:
-			return 'Database Error'
-
+		elif request.form['usertype'] == 'user':
+			user = User.query.filter_by(user=request.form['username']).first()
+			password = User.query.filter_by(password=request.form['password']).first()
+			if user.id==password.id:
+				return redirect('/user')
+			else:
+				return "incorrect username password pair"
+		else:
+			return "Error logging in"
 	else:
-
-		tasks = Todo.query.order_by(Todo.date_created).all()
-
-		return render_template('index.html', tasks=tasks)
-
-@app.route('/delete/<int:id>')
-
-def delete(id):
-	task_to_delete = Todo.query.get_or_404(id)
-
-	try:
-		db.session.delete(task_to_delete)
-		db.session.commit()
-
-		return redirect('/')
-
-	except:
-
-		return 'Error deleting'
+		return render_template('login.html')
 
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 
-def update(id):
+def signup():
 
-	task = Todo.query.get_or_404(id)
+	# task = Todo.query.get_or_404(id)
+
+	# if request.method == 'POST':
+	# 	task.content = request.form['content']
+	# 	try:
+	# 		db.session.commit()
+	# 		return redirect('/')
+	# 	except:
+	# 		return "Update Error"
+	# else:
 
 	if request.method == 'POST':
-		task.content = request.form['content']
-		try:
-			db.session.commit()
-			return redirect('/')
-		except:
-			return "Update Error"
+		if request.form['usertype'] == 'admin':
+			admin = request.form['username']
+			pwd = request.form['password']
+			repwd = request.form['repassword']
+			if pwd == repwd:
+				newAdmin = Admin(admin=admin, password=pwd)
+			else:
+				return "both passwords are different"
+
+			try:
+				db.session.add(newAdmin)
+				db.session.commit()
+
+				return redirect('/login')
+
+			except:
+				return 'Database Error'
+
+		elif request.form['usertype'] == 'user':
+			user = request.form['username']
+			pwd = request.form['password']
+			repwd = request.form['repassword']
+			if pwd == repwd:
+				newUser = User(user=user, password=pwd)
+			else:
+				return "both passwords are different"
+
+			try:
+				db.session.add(newAdmin)
+				db.session.commit()
+
+				return redirect('/login')
+
+			except:
+				return 'Database Error'
 	else:
-		return render_template('update.html', task=task)
+		return render_template('signup.html')
 
 
 
